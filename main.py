ae0918ef9b1810e7975216d5a119a9b3f715155a
Computer <computer@Computers-MacBook-Pro.local>
2025-05-17 19:44:59 +0400

    url scraper

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_page(url, output_file="scraped_rls.txt"):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    tags = soup.find_all("a")
+
+    with open(output_file, "w", encoding='utf-8') as f:
+        for tag in tags:
+            href = tag.get("href", "")
+            if href and href.startswith("https"):
+                f.write(href + "\n")
+
+
+get_page(input("Paste url to scrape "))
