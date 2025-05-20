1b65ed17589ca6da38f80a058e5d308a3b2abe1d
Computer <computer@Computers-MacBook-Pro.local>
2025-05-20 19:14:18 +0400

    update keyword search

---
+++ advanced.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+visited_urls = set()
+
+def spider_urls(url, keyword, output_file="scraped_rls.txt"):
+    
+    try:
+        response = requests.get(url)
+    except:
+        print(f"Request failed {url}")
+        return
+
+
+    soup =  BeautifulSoup(response.content, 'html.parser')
+
+    tags = soup.find_all("a")
+
+    with open(output_file, "w", encoding='utf-8') as f:
+       for tag in tags:
+            href = tag.get("href", keyword)
+            if href and href.startswith("https"):
+                f.write(href + "\n") 
+
+
+
+
+
+
+
+
+
+
+url = input("Enter url to scrape  ")
+keyword = input("Enter the keyword")
+spider_urls(url, keyword)
